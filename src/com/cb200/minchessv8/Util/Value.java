1b279ce269915e2799a40d5bcb98c419233abdd7
chuckyboy200 <60534010+chuckyboy200@users.noreply.github.com>
2022-12-01 14:21:29 +1300

    add new Value class

---
+++ src/com/cb200/minchessv8/Util/Value.java
@@ -0,0 +1,18 @@
+package com.cb200.minchessv8.Util;
+
+public class Value {
+
+    public final static int INVALID = -1;
+	public final static int EMPTY = 0;
+	public final static int NONE = 0;
+	public final static int WHITE = 0;
+	public final static int WHITE_BIT = 0;
+	public final static int BLACK = 1;
+	public final static int BLACK_BIT = 8;
+	public final static int[] KINGSIDE_BIT = { 1, 4 };
+	public final static int[] QUEENSIDE_BIT = { 2, 8 };
+
+    private Value() {}
+
+    
+}
